85d44c80845158aaf6dafff1ed6f1d3389814e8e
Felipe de Souza <systen3@gmail.com>
2021-12-16 08:17:53 -0300

    input e data binding

--- 1_instalacao_vue/src/app.js
+++ 1_instalacao_vue/src/app.js
@@ -1,8 +1,19 @@
 const MyNameApp = {
     data() {
         return {
-            name: "Felipe",
-            age: 30
+            name: "",
+            age: 30,
+            input_name: ""
+        }
+    },
+    methods: {
+        submitForm(e) {
+
+            e.preventDefault();
+
+            console.log(this.input_name);
+
+            this.name = this.input_name;
         }
     }
 }
