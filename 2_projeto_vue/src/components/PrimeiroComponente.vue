a545f9dc6ec0773df5dbefae525ece11a471fc6a
Felipe de Souza <systen3@gmail.com>
2021-12-26 08:34:03 -0300

    componentes reutilizacao

--- 2_projeto_vue/src/components/PrimeiroComponente.vue
+++ 2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -1,19 +1,26 @@
 <template>
     <div>
+          <Reutilizacao />
     <h1>Hola Vue!</h1>
+      <Reutilizacao />
     <p> Meu nome é {{ nome }} e trabalho como {{ profissao }} </p>
     <MultiplosEventos />
+    <Reutilizacao />
+      <Reutilizacao />
+        <Reutilizacao />
     </div>
 </template>
 
 <script>
 import MultiplosEventos from './MultiplosEventos.vue';
+import Reutilizacao from './Reutilizacao.vue';
 
 
 export default {
     name: 'PrimeiroComponente',
     components : {
-        MultiplosEventos
+        MultiplosEventos,
+        Reutilizacao
     },
     data() {
         return {
